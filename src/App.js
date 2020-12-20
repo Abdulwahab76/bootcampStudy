13c86a802fa89e3cbb4803cf7a2dc95ec823e90d
Abdulwahab76 <abdulwahab12.pk@gmail.com>
2020-12-20 13:14:50 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
                     target="_blank"
                     rel="noopener noreferrer"
                 >
-                    Learn React wahab
+                    Learn React wahab hello
                 </a>
             </header>
         </div>
